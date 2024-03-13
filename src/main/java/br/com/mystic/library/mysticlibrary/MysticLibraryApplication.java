3c88fcb03897be9d77b51e94ccb74344f8cec89d
thomasmaffezzolli8@gmail.com <thomas.maffezzolli@catolicasc.edu.br>
2024-03-13 20:36:42 -0300

    first commit

---
+++ src/main/java/br/com/mystic/library/mysticlibrary/MysticLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.mystic.library.mysticlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysticLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysticLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mystic/library/mysticlibrary/MysticLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mystic.library.mysticlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysticLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
