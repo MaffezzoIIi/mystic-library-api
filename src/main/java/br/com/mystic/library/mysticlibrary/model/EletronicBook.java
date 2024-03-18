9c71a355ddf90d3d884ba14c3f2abed50594906f
Thomas Maffezzolli <thomasmaffezzolli8@gmail.com>
2024-03-18 20:24:25 -0300

    Correção de erros

--- src/main/java/br/com/mystic/library/mysticlibrary/model/EletronicBook.java
+++ src/main/java/br/com/mystic/library/mysticlibrary/model/EletronicBook.java
@@ -1,37 +1,20 @@
 package br.com.mystic.library.mysticlibrary.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 
 import java.util.List;
 
 @Entity
-public class EletronicBook extends Book{
-
-    @Id
-    @GeneratedValue
-    private Long id;
+public class EletronicBook extends Book {
 
     private String format;
 
     public EletronicBook() {
     }
 
-    public EletronicBook(Long id, String title, List<Author> author, Publishing publishing, Long id1, String format) {
+    public EletronicBook(Long id, String title, List<Author> author, Publishing publishing, String format) {
         super(id, title, author, publishing);
-        this.id = id1;
         this.format = format;
-    }
-
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFormat() {
